35adcd421e7db93ef428af5900e7d148c3379dcc
Trey Kemp <kemptrey@outlook.com>
2022-02-12 14:47:06 -0700

    added comments for script

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+//[array]
 const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+//loop through each sound, create a button, give it a class of btn,
+//set inner text to the sound itself..
+//insert the button and style them with append child and pass in the button
 
 sounds.forEach((sound) => {
   const btn = document.createElement("button");
@@ -6,22 +11,27 @@ sounds.forEach((sound) => {
 
   btn.innerText = sound;
 
+  //listen for on click, on click run the function
   btn.addEventListener("click", () => {
     //this will stop the previous sound from playing
     stopSongs();
-    //this will play the songs by finding the div by ID
-
+    //this will play the audio
     document.getElementById(sound).play();
   });
 
   document.getElementById("buttons").appendChild(btn);
 });
 
+//
 function stopSongs() {
+  //for each sound, create a variable called 'song' and select the particular sound
+  //
   sounds.forEach((sound) => {
     const song = document.getElementById(sound);
 
+    //pause the sound
     song.pause();
+    //grab current time and reset it to 0
     song.currentTime = 0;
   });
 }
